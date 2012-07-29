0ffffba6b7aedc85d452c6f3d2e45ef2b01df7e4
Priyesh Patel <priyesh@ukawala.com>
2012-07-29 15:52:46 +0100

    corrected extensions

---
+++ venv_analyse.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+WORKING_DIR=./
+ACTIVATE_PATH=./venv/bin/activate
+MY_PATH="`dirname \"$0\"`"
+MY_PATH="`( cd \"$MY_PATH\" && pwd)`"
+
+cd $MY_PATH
+cd $WORKING_DIR
+. $ACTIVATE_PATH
+exec python analyse.py
