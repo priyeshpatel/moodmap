844ec612e266ac6c4f7bfe8668d5674026cdb810
Priyesh Patel <priyesh@ukawala.com>
2013-02-05 16:08:18 +0000

    added validate_doc_update function

--- couchdb/moodmap_tweets.js
+++ couchdb/moodmap_tweets.js
@@ -1,10 +1,10 @@
 {
     "_id": "_design/moodmap_tweets", 
-    "_rev": "1-XXX",
     "language": "javascript", 
     "views": {
         "tweets": {
             "map": "function(doc) {\n  emit(doc.id, [doc.tweet, doc.rating]);\n}"
         }
-    }
+    },
+    "validate_doc_update": "function(newDoc, oldDoc, userCtx) { if (userCtx.roles.indexOf('_admin') !== -1 || userCtx.name == \"moodmap\") { return; } else { throw({forbidden: 'You are not authorized to edit this db.'}); } }"
 }
